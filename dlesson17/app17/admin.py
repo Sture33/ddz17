372b3138dc3d680ba302720f8c743b8eb20249c7
Sture33 <119112960+Sture33@users.noreply.github.com>
2024-01-15 15:30:53 +0500

    d

---
+++ dlesson17/app17/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from app17.models import *
+
+# Register your models here.
+admin.site.register(Task)
+admin.site.register(ActionType)
+admin.site.register(Actions)
